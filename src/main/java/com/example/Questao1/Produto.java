77c597e20c6eca412f0c055ae17f90bb234dae89
Edvan <emanuel.edvan08@gmail.com>
2024-05-30 22:53:54 -0300

    primeiro commit

---
+++ src/main/java/com/example/Questao1/Produto.java
@@ -0,0 +1,45 @@
+package com.example.Questao1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+	    @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)	
+		private Long id;
+	    private String nome;
+	    private double preco;
+	    
+	    public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public double getPreco() {
+			return preco;
+		}
+		public void setPreco(double preco) {
+			this.preco = preco;
+		}
+		
+		 public Produto() {
+		    }
+		 public Produto(String nome, double preco) {
+		        this.nome = nome;
+		        this.preco = preco;
+		    }
+
+}
